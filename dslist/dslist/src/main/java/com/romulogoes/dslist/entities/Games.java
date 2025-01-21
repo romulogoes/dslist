a70ff16969e77b590010d6d923da59659a7a84e0
Rômulo Góes <romulogoesfts@gmail.com>
2025-01-20 22:08:05 -0400

    Game seed

--- dslist/dslist/src/main/java/com/romulogoes/dslist/entities/Games.java
+++ dslist/dslist/src/main/java/com/romulogoes/dslist/entities/Games.java
@@ -22,22 +22,28 @@ public class Games {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Games() {
 	}
 
-	public Games(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Games(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platform = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -75,12 +81,21 @@ public class Games {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platform) {
+		this.platforms = platforms;
+	}
+	
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
